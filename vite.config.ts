548c641b41b8b6457a88f0d6ecee08b11aaa24c1
Nikita Chernov <drimchansky@gmail.com>
2025-08-23 14:28:51 +0400

    Add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
   server: {
     port: 5757
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
